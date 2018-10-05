e94f6db1e3b5d0e718ed1189b2faf1979ad2fad3
cailee tracytest3 <cailee+tracytest3@flatironschool.com>
2018-10-05 16:08:44 +0000

    automatically backed up by learn

--- app/controllers/pigeons_controller.rb
+++ app/controllers/pigeons_controller.rb
@@ -1,2 +1,48 @@
-# create your PigeonsController here
-# it should inherit from ApplicationController
+class PigeonsController < ApplicationController
+
+  get '/pigeons' do
+    @pigeons = Pigeon.all
+    erb :"pigeons/index"
+  end
+
+  get '/pigeons/new' do
+    erb :"pigeons/new"
+  end
+
+  get '/pigeons/:id' do
+    @pigeon = Pigeon.find(params[:id])
+    erb :"pigeons/show"
+  end
+
+  get '/pigeons/:id/edit' do
+    @pigeon = Pigeon.find(params[:id])
+    erb :"pigeons/edit"
+  end
+
+  post '/pigeons' do
+    @pigeon = Pigeon.new(params)
+    @pigeon.name = params["name"]
+    @pigeon.color = params["color"]
+    @pigeon.gender = params["gender"]
+    @pigeon.lives = params["lives"]
+    @pigeon.save
+    redirect "/pigeons"
+  end
+
+  patch '/pigeons/:id' do
+    @pigeon = Pigeon.find(params[:id])
+    @pigeon.name = params["name"]
+    @pigeon.color = params["color"]
+    @pigeon.gender = params["gender"]
+    @pigeon.lives = params["lives"]
+    @pigeon.save
+    redirect "/pigeons/#{@pigeon.id}"
+  end
+
+  delete '/pigeons/:id' do
+    @pigeon = Pigeon.find(params[:id])
+    @pigeon.destroy
+    redirect "/pigeons"
+  end
+
+end
